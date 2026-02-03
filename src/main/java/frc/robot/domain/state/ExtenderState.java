4941ba0f7fd7f8bcded183d66f4e1e4daf0b720d
zuiken-code <zuiken2022robo@gmail.com>
2026-02-04 00:48:19 +0900

    add state

---
+++ src/main/java/frc/robot/domain/state/ExtenderState.java
@@ -0,0 +1,8 @@
+package frc.robot.domain.state;
+
+import edu.wpi.first.math.geometry.Pose2d;
+
+public class ExtenderState {
+    /** スタート地点を原点としたロボットの位置と回転 */
+    public static Pose2d drivePosition;
+}
