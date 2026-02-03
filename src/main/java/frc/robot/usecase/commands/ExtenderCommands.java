e39b6665050789b63c4b5caf1e33e623d90205b1
zuiken-code <zuiken2022robo@gmail.com>
2026-02-04 00:53:13 +0900

    add commands

---
+++ src/main/java/frc/robot/usecase/commands/ExtenderCommands.java
@@ -0,0 +1,16 @@
+package frc.robot.usecase.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.domain.repository.TemplateRepository;
+
+public class ExtenderCommands {
+    private static TemplateRepository templateRepository;
+
+    public static void init(TemplateRepository tr) {
+        templateRepository = tr;
+    }
+
+    public static Command templateCommand() {
+        return templateRepository.run(()->{});
+    }
+}
