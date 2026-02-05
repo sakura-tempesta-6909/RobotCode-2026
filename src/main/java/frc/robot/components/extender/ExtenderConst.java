168afe741f849736976291346f19e041d04a9130
Rei6627 <rei.20100331@gmail.com>
2026-02-05 12:51:44 +0900

    add ExtenderConst

--- src/main/java/frc/robot/components/extender/ExtenderConst.java
+++ src/main/java/frc/robot/components/extender/ExtenderConst.java
@@ -1,5 +1,9 @@
 package frc.robot.components.extender;
 
 public final class ExtenderConst {
+    public static final class Ports{
+        public static final int ExtenderMotor = 14;
+
+    }
     
 }
