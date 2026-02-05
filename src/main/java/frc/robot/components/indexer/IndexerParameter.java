dd718fdbc21d55b64be9883194c6e0d543813731
zuiken-code <zuiken2022robo@gmail.com>
2026-02-05 17:13:44 +0900

    fix Speed

--- src/main/java/frc/robot/components/indexer/IndexerParameter.java
+++ src/main/java/frc/robot/components/indexer/IndexerParameter.java
@@ -1,7 +1,7 @@
 package frc.robot.components.indexer;
 
 public final class IndexerParameter {
-    public static final class speed{
+    public static final class Speed{
         /** Shooterに送る時のスピード | 送る方に回す方向が正 | [0, 1]*/
         public static final double IndexerRollerFeedSpeed = 0.5;
         /** 逆回転する時のスピード | shooterに送るのと逆の方向なので負 | [-1, 0]*/
