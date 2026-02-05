0d0b4e3f65f7c57ef9bc7748800b2580d379e2ad
zuiken-code <zuiken2022robo@gmail.com>
2026-02-06 00:10:51 +0900

    add prot

--- src/main/java/frc/robot/components/indexer/IndexerConst.java
+++ src/main/java/frc/robot/components/indexer/IndexerConst.java
@@ -3,6 +3,6 @@ package frc.robot.components.indexer;
 public final class IndexerConst {
     public static final class Ports{
         /** Indexerのモーターのポート番号*/
-        public static final int IndexerMotor = 0;
+        public static final int IndexerMotor = 15;
     }
 }
