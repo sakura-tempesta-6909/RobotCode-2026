990557dad182b8f4a54775ac45522888cd751c65
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2026-02-05 12:33:01 +0900

    add Repository

--- src/main/java/frc/robot/domain/repository/LEDRepository.java
+++ src/main/java/frc/robot/domain/repository/LEDRepository.java
@@ -3,5 +3,9 @@ package frc.robot.domain.repository;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public interface LEDRepository extends Subsystem {
+    /** LEDを任意の色に変える */
+    void changeLight(int red,int green, int blue);
+    /** LEDを任意の色で点滅させる */
+    void flashLight(int red,int green, int blue);
 
 }
