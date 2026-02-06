3158e8941a53383c75b583605ef75efa20da300f
Kanna <doraemon.bd903@gmail.com>
2026-02-06 18:55:34 +0900

    説明を追加

--- src/main/java/frc/robot/domain/state/ShooterState.java
+++ src/main/java/frc/robot/domain/state/ShooterState.java
@@ -1,12 +1,21 @@
 package frc.robot.domain.state;
 
 public class ShooterState {
-    /** シュート時のモーターのスピード */
+    /** シュート時のモーターのスピード 
+     * -1～1の範囲（負方向の最大出力―1から正方向の最大出力1まで）
+    */
     public static double motorSpeed;
-    /** シューターのモーターが動作しているか */
+
+    /** シューターのモーターが動作しているか 
+     * Boolean型 True動作中 False停止中
+    */
     public static boolean isMotorActive;
+
     /** シューターの目標RPM */
     public static double targetMotorSpeed;
-    /** シューターのモーターが十分な回転数に達しているかどうか */
+
+    /** シューターのモーターが十分な回転数に達しているかどうか 
+     * Boolean型 True達している False未達
+    */
     public static boolean isReadyToShoot;
 }
