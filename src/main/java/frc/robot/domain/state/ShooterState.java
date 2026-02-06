faa4d5c4c28df4a794b8b2e75fe73f954cca96ee
Kanna <doraemon.bd903@gmail.com>
2026-02-06 18:36:39 +0900

    Stateの定義

--- src/main/java/frc/robot/domain/state/ShooterState.java
+++ src/main/java/frc/robot/domain/state/ShooterState.java
@@ -1,7 +1,12 @@
 package frc.robot.domain.state;
 
-import edu.wpi.first.math.geometry.Pose2d;
-
 public class ShooterState {
-
+    /** シュート時のモーターのスピード */
+    public static double motorSpeed;
+    /** シューターのモーターが動作しているか */
+    public static boolean isMotorActive;
+    /** シューターの目標RPM */
+    public static double targetMotorSpeed;
+    /** シューターのモーターが十分な回転数に達しているかどうか */
+    public static boolean isReadyToShoot;
 }
