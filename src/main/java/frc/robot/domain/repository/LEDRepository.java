1880dfbdb2a4f1e14a5d1acff40c1850798be845
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2026-02-06 23:16:31 +0900

    add space

--- src/main/java/frc/robot/domain/repository/LEDRepository.java
+++ src/main/java/frc/robot/domain/repository/LEDRepository.java
@@ -4,8 +4,8 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public interface LEDRepository extends Subsystem {
     /** LEDを任意の色に変える */
-    void changeLight(int red,int green, int blue);
+    void changeLight(int red, int green, int blue);
     /** LEDを任意の色で点滅させる */
-    void flashLight(int red,int green, int blue);
+    void flashLight(int red, int green, int blue);
 
 }
