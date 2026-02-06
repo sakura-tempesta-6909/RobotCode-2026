3a316e165ca86b6684347406acf01a7e045844c3
Kanna <doraemon.bd903@gmail.com>
2026-02-06 19:10:10 +0900

    説明を追加

--- src/main/java/frc/robot/domain/repository/ShooterRepository.java
+++ src/main/java/frc/robot/domain/repository/ShooterRepository.java
@@ -5,12 +5,12 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 public interface ShooterRepository extends Subsystem {
 
     /** MaxRPMに割合をかけてVelocity制御で動かす 
-     * 範囲：-1～1
+     * 範囲：-1～1　（負方向の最大出力―1から正方向の最大出力1まで）
     */
     void moveShooterSpecifiedSpeed(double targetSpeed);
 
     /** PercentOutputでモーターを任意の速度で動かす
-     * 範囲：-1～1
+     * 範囲：-1～1 （負方向の最大出力―1から正方向の最大出力1まで）
      */
     void moveShooterSpecifedPower(double targetPower);
 
