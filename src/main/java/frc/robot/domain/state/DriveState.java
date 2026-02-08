d4c4e56f55786c62eeeaadf2410fd1b5aa5086d3
Rei6627 <rei.20100331@gmail.com>
2026-02-08 21:35:31 +0900

    fix DriveState

--- src/main/java/frc/robot/domain/state/DriveState.java
+++ src/main/java/frc/robot/domain/state/DriveState.java
@@ -3,10 +3,8 @@ package frc.robot.domain.state;
 import edu.wpi.first.math.geometry.Pose2d;
 
 public class DriveState {
-    /** スタート地点を原点(0,0)としたロボットの位置(x[m],y[m])|x軸:フィールドを上から見て奥方向（前方向）を正,Y軸:上から見てフィールドのを左方向を正  */
+    /** スタート地点（上から見て右下）を原点(0,0)としたロボットの位置(x[m],y[m])とフィールド上の前向きを0としたロボットの角度[degree]|x軸:フィールドを上から見て奥方向（前方向）を正,Y軸:上から見てフィールドのを左方向を正,角度：ロボットを上から見て反時計回りが正|PathPlannerの座標と一致 */
     public static Pose2d drivePosition;
-    /** フィールド上の前向きを0としたロボットの角度|単位は度数法|ロボットを上から見て反時計回りが正  */
-    public static double currentAngle;
     /** 停止状態を0としたロボットの速度[m/s] */
     public static double driveSpeed;
     /** ロボットの、x軸方向の運動が0の時を0とした、ロボットのX軸方向の速度[m/s] */
