3706aa119e2d6edd32887ef2ca8eb8f4469a7ab0
Rei6627 <rei.20100331@gmail.com>
2026-02-08 21:26:28 +0900

    fix Big letter

--- src/main/java/frc/robot/components/extender/ExtenderConst.java
+++ src/main/java/frc/robot/components/extender/ExtenderConst.java
@@ -3,7 +3,7 @@ package frc.robot.components.extender;
 public final class ExtenderConst {
     public static final class Ports{
         /** extenderのモーターのCANID */
-        public static final int ExtenderMotor = 14;
+        public static final int extenderMotor = 14;
         /** extenderの展開の上限と下限のスイッチ */
         public static final int upperExtenderLimitSwitch = 1;
         public static final int lowerExtenderLimitSwitch = 2;
--- src/main/java/frc/robot/domain/state/ExtenderState.java
+++ src/main/java/frc/robot/domain/state/ExtenderState.java
@@ -3,7 +3,5 @@ package frc.robot.domain.state;
 import edu.wpi.first.math.geometry.Pose2d;
 
 public class ExtenderState {
-    
+
 }
-
-
