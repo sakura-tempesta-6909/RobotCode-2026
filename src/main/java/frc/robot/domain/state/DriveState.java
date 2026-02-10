c06bf695b323282c02925117ee1eb9057c568c32
Rei6627 <rei.20100331@gmail.com>
2026-02-10 19:45:40 +0900

    fix drivestate

--- src/main/java/frc/robot/domain/state/DriveState.java
+++ src/main/java/frc/robot/domain/state/DriveState.java
@@ -1,18 +1,15 @@
 package frc.robot.domain.state;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 public class DriveState {
     /** スタート地点（上から見て右下）を原点(0,0)としたロボットの位置(x[m],y[m])とフィールド上の前向きを0としたロボットの角度[degree]|x軸:フィールドを上から見て奥方向（前方向）を正,Y軸:上から見てフィールドのを左方向を正,角度：ロボットを上から見て反時計回りが正|PathPlannerの座標と一致 */
     public static Pose2d drivePosition;
-    /** 停止状態を0としたロボットの速度[m/s] */
+    /** 停止状態を0としたロボットの速度[m/s] */ 
     public static double driveSpeed;
-    /** ロボットの、x軸方向の運動が0の時を0とした、ロボットのX軸方向の速度[m/s] */
-    public static double driveXSpeed;
-    /** ロボットの、y軸方向の運動が0の時を0とした、ロボットのY軸方向の速度[m/s] */
-    public static double driveYSpeed;
-    /** ロボットが回転していない時を0とした、ロボットの回転速度[rad/s] |フィールドを上から見て反時計回りが正 */
-    public static double driveRotationSpeed;
+    /** ロボットの、(x,y)軸方向の運動が0の時を0とした、ロボットの(X,Y)軸方向の速度[m/s]とロボットが回転していない時を0とした、ロボットの回転速度[rad/s] |フィールドを上から見て反時計回りが正 */
+    public static ChassisSpeeds driveXYOmegaSpeed;
     /** ロボットがシュート可能な位置かどうか |可能->true,不可->false */
     public static boolean isShootPosition;
 }
