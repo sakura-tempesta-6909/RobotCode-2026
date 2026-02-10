bf5b15d20649fc578a6e220b2872bdc67476be3a
Rei6627 <rei.20100331@gmail.com>
2026-02-10 20:44:32 +0900

    add ExtenderRepository

--- src/main/java/frc/robot/components/extender/infrastructure/Extender.java
+++ src/main/java/frc/robot/components/extender/infrastructure/Extender.java
@@ -13,5 +13,25 @@ public class Extender implements ExtenderRepository {
     @Override
     public void periodic() {
     }
+
+    @Override
+    public void moveExtenderSpecifiedAngle(double targetAngle) {
+
+    }
+
+    @Override
+    public void moveIndexerSpecifiedSpeed(double targetSpeed) {
+
+    }
+
+    @Override
+    public void resetPID() {
+
+    }
+
+    @Override
+    public void resetEncorder() {
+        
+    }
     
 }
--- src/main/java/frc/robot/domain/repository/ExtenderRepository.java
+++ src/main/java/frc/robot/domain/repository/ExtenderRepository.java
@@ -3,5 +3,17 @@ package frc.robot.domain.repository;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public interface ExtenderRepository extends Subsystem {
+    
+    /** Extenderを任意の角度に動かす(Position)  */
+    void moveExtenderSpecifiedAngle(double targetAngle);
+
+    /** Extenderを任意の速度で動かす(PercentOutput)  */
+    void moveIndexerSpecifiedSpeed(double targetSpeed);
+
+    /** PIDをリセットする */
+    void resetPID();
+
+    /** encoderをリセットする */
+    void resetEncorder();
 
 }
