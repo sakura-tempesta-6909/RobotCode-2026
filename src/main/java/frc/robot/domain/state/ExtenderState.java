c0a5c1e2cb2665c9205014d63fc4ba1670bd493b
Rei6627 <rei.20100331@gmail.com>
2026-02-10 19:36:56 +0900

    fix extenderstate

--- src/main/java/frc/robot/domain/state/ExtenderState.java
+++ src/main/java/frc/robot/domain/state/ExtenderState.java
@@ -5,7 +5,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 public class ExtenderState {
     /** Extenderのモーターが動作しているか|動いている->true,停止->false*/
     public static boolean isMotorActive;
-    /** 起動時の収納時を0度としたExtenderの角度[degree]|0<=currentAngle<=90(展開時に90度になる) */
+    /** 底面が地面と並行な場合を0度としたExtenderの角度[degree]|0<=currentAngle<=90(初期時に底面が地面と垂直なため90度になる) */
     public static double currentAngle;
     /** intakeできる位置にExtenderがあるかないか|可能->true,不可->false */
     public static boolean isIntakeAngle;
