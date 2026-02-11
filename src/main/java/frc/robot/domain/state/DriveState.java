19ae6fe14e0c175602c6ac05b5dc3d0f5416b2da
Rei6627 <rei.20100331@gmail.com>
2026-02-11 21:52:18 +0900

    delete trash

--- src/main/java/frc/robot/domain/state/DriveState.java
+++ src/main/java/frc/robot/domain/state/DriveState.java
@@ -8,8 +8,6 @@ public class DriveState {
      * |x軸:フィールドを上から見て奥方向（前方向）を正,Y軸:上から見てフィールドのを左方向を正,角度：ロボットを上から見て反時計回りが正
      * |PathPlannerの座標と一致 (参照→)https://docs.wpilib.org/en/stable/docs/software/basic-programming/coordinate-system.html*/
     public static Pose2d drivePosition;
-    /** 停止状態を0としたロボットの速度[m/s] */ 
-    public static double driveSpeed;
     /** ロボットの、(x,y)軸方向の運動が0の時を0とした、ロボットの(X,Y)軸方向の速度[m/s]とロボットが回転していない時を0とした、ロボットの回転速度[rad/s] |フィールドを上から見て反時計回りが正 */
     public static ChassisSpeeds driveXYOmegaSpeed;
     /** ロボットがシュート可能な位置かどうか |可能->true,不可->false */
