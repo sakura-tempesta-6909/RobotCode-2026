a51eb2e79a134a0a75fffc2226f9047a8354fc20
Rei6627 <rei.20100331@gmail.com>
2026-02-11 21:36:47 +0900

    ExtenderRepository

--- src/main/java/frc/robot/domain/repository/ExtenderRepository.java
+++ src/main/java/frc/robot/domain/repository/ExtenderRepository.java
@@ -7,8 +7,8 @@ public interface ExtenderRepository extends Subsystem {
     /** Extenderを任意の角度に動かす(Position) |targetAngle:Extenderが地面に対して並行な時を0とした目標の角度[degree]|地面に対して上に動かす方向を正 */
     void moveExtenderSpecifiedAngle(double targetAngle);
 
-    /** Extenderを任意の速度で動かす(PercentOutput) |targetspeed: 止まっている時を0とした目標のスピード(％[-1~1])|地面に対して上に動かす方向を正 */
-    void moveIndexerSpecifiedSpeed(double targetSpeed);
+    /** Extenderを任意の力で動かす(PercentOutput) |targetspeed: 止まっている時を0とした力(％[-1~1])|地面に対して上に動かす方向を正 */
+    void moveIndexerSpecifiedPower(double targetPower);
 
     /** PIDをリセットする */
     void resetPID();
