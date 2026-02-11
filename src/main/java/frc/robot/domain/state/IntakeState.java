cf273d7e161d7df0612ca9f1fd17a6633ce8b0cc
taku-code3 <takuya39117@gmail.com>
2026-02-11 22:13:12 +0900

    再修正

--- src/main/java/frc/robot/domain/state/IntakeState.java
+++ src/main/java/frc/robot/domain/state/IntakeState.java
@@ -1,8 +1,8 @@
 package frc.robot.domain.state;
 
 public class IntakeState {
-    //**モーターのスピード | インテークに取り込む方向を正 | [-1, 1] */
+    /**モーターのスピード | インテークに取り込む方向を正 | [-1, 1] */
     public static double motorSpeed;
-    //**モーターが動作しているか | 動いてたらtrue*/
+    /**モーターが動作しているか | 動いてたらtrue*/
     public static boolean isMotorActive;
 }
