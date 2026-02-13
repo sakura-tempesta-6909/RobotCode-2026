1e870ad23801df73c781360c002b7c5897b11700
Kanna <doraemon.bd903@gmail.com>
2026-02-13 18:33:42 +0900

    正回転でシュート

--- src/main/java/frc/robot/domain/repository/ShooterRepository.java
+++ src/main/java/frc/robot/domain/repository/ShooterRepository.java
@@ -7,6 +7,7 @@ public interface ShooterRepository extends Subsystem {
     /** 
      * Velocity制御でモーターを任意の速度で動かす 
      * 範囲：-1～1　（負方向の最大回転数割合―1～正方向の最大回転数割合1まで）
+     * 正回転でシュート
      * 0で停止
     */
     void moveShooterSpecifiedSpeed(double targetSpeed);
@@ -14,6 +15,7 @@ public interface ShooterRepository extends Subsystem {
     /** 
      * PercentOutputでモーターを任意の速度で動かす
      * 範囲：-1～1 （負方向の最大出力割合―1～正方向の最大出力割合1まで）
+     * 正回転でシュート
      * 0で停止
      */
     void moveShooterSpecifedPower(double targetPower);
