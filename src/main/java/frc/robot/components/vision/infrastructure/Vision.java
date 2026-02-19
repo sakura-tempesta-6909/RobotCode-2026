101037a72134639a369ba24c7dbf434d8f3e281a
zuiken-code <zuiken2022robo@gmail.com>
2026-02-19 20:06:13 +0900

    add Vision Repository

--- src/main/java/frc/robot/components/vision/infrastructure/Vision.java
+++ src/main/java/frc/robot/components/vision/infrastructure/Vision.java
@@ -1,10 +1,18 @@
 package frc.robot.components.vision.infrastructure;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import frc.robot.domain.repository.VisionRepository;
+import frc.robot.domain.repository.VisionRepository.Data;
+import org.photonvision.PhotonPoseEstimator;
 
 public class Vision implements VisionRepository {
 
     public Vision() {
+    }
+
+    @Override
+    public Data updateVision(PhotonPoseEstimator estimator){
+        return new Data(new Pose2d(), 0.0);
     }
 
     /**
--- src/main/java/frc/robot/domain/repository/VisionRepository.java
+++ src/main/java/frc/robot/domain/repository/VisionRepository.java
@@ -1,7 +1,15 @@
 package frc.robot.domain.repository;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import org.photonvision.PhotonPoseEstimator;
 
 public interface VisionRepository extends Subsystem {
 
+    record Data(Pose2d estimatedPose, double timeStanp) {}
+    /**
+     * 引数に追加されたphotonPoseEstimatorからPoseとtimestanpの情報を抜き取る
+     * @param estimator photonPoseEstimator
+     */
+    Data updateVision(PhotonPoseEstimator estimator);
 }
