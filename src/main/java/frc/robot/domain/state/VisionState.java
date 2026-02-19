aff2694bfbd705d009e37645fcae56c19d445143
zuiken-code <zuiken2022robo@gmail.com>
2026-02-19 16:48:52 +0900

    add state

--- src/main/java/frc/robot/domain/state/VisionState.java
+++ src/main/java/frc/robot/domain/state/VisionState.java
@@ -3,5 +3,20 @@ package frc.robot.domain.state;
 import edu.wpi.first.math.geometry.Pose2d;
 
 public class VisionState {
-
+    /** 左カメラのみから計算したロボットの位置*/
+    public static Pose2d leftCameraPose;
+    /** 右カメラのみから計算したロボットの位置*/
+    public static Pose2d rightCameraPose;
+    /** 左のカメラのタイムスタンプ*/
+    public static double leftCameraTimestamp;
+    /** 右のカメラのタイムスタンプ*/
+    public static double rightCameraTimestamp;
+    /** 左のカメラが接続されているか*/
+    public static boolean leftCameraConnected;
+    /** 右のカメラが接続されているか*/
+    public static boolean rightCameraConnected;
+    /** 左のカメラにタグが映ってるか*/
+    public static boolean leftHasTarget;
+    /** 右のカメラにタグが映ってるか*/
+    public static boolean rightHasTarget;
 }
