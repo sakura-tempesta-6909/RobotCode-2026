6d96ead4d7eddefe1a7e4ce05bf361275a8ab463
Kanna <doraemon.bd903@gmail.com>
2026-02-20 21:27:39 +0900

    修正

--- src/main/java/frc/robot/mode/DriveMode.java
+++ src/main/java/frc/robot/mode/DriveMode.java
@@ -5,6 +5,7 @@ import frc.robot.domain.option.DriveOption;
 import frc.robot.domain.option.DriveOption.DriveOriented;
 import frc.robot.domain.option.DriveOption.DriveSpeed;
 import frc.robot.usecase.commands.DriveCommands;
+import frc.robot.usecase.commands.ShooterCommands;
 
 
 public class DriveMode extends Mode {
@@ -26,5 +27,17 @@ public class DriveMode extends Mode {
         configureDefault();
 
         driveController.rightBumper().whileTrue(DriveOption.driveOriented.set(DriveOriented.s_robotOriented));
+
+                
+        operateController.a().onTrue(ShooterCommands.shootToHub(() -> 1.0));
+        operateController.b().onTrue(ShooterCommands.feed(() -> 0.8));
+        operateController.x().onTrue(ShooterCommands.reverseShooter(() -> -0.4));
+        operateController.y().onTrue(ShooterCommands.moveShooterSpecifiedSpeed(() -> 0.5));
+
+        //実験用に
+        operateController.leftBumper().onTrue(ShooterCommands.moveShooterSpecifiedSpeed(() -> 0.3));
+        operateController.rightBumper().onTrue(ShooterCommands.moveShooterSpecifiedSpeed(() -> 0.7));
+        operateController.leftTrigger().whileTrue(ShooterCommands.reverseShooter(() -> -0.4));
+        operateController.rightTrigger().whileTrue(ShooterCommands.shootToHub(() -> 1.0));
     }
 }
