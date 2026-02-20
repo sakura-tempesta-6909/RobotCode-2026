ae940c14ce23027ed532c50e7fbf0bc00c9e390b
Rei6627 <rei.20100331@gmail.com>
2026-02-20 18:59:32 +0900

    add comment out

--- src/main/java/frc/robot/components/extender/ExtenderParameter.java
+++ src/main/java/frc/robot/components/extender/ExtenderParameter.java
@@ -1,6 +1,8 @@
 package frc.robot.components.extender;
 
 public final class ExtenderParameter {
+    /** デフォルトの状態の角度 */
     public static final double InitialAngle = 90;
+    /** Intakeできる角度 */
     public static final double IntakeAngle = 0;
 }
