5fae2d5ef02e99d501a89b8aff374bccbb7bcf8f
taku-code3 <takuya39117@gmail.com>
2026-02-21 18:00:07 +0900

    Revert "IntakeRepository"
    This reverts commit ca8e080a0c778d01a32d002f59827e59ed95a1a8.

--- src/main/java/frc/robot/components/intake/infrastructure/Intake.java
+++ src/main/java/frc/robot/components/intake/infrastructure/Intake.java
@@ -7,9 +7,6 @@ public class Intake implements IntakeRepository {
     public Intake() {
     }
 
-    @Override
-    public void moveIntakeSpecifiedSpeed(double targetSpeed){     
-    }
     /**
      * Stateへの書き込みを行う
      */
