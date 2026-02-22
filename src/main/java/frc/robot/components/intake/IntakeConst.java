097869adb6f207ae6f72005dff646b2b09794a1d
taku-code3 <takuya39117@gmail.com>
2026-02-22 19:57:47 +0900

    Add Infra

--- src/main/java/frc/robot/components/intake/IntakeConst.java
+++ src/main/java/frc/robot/components/intake/IntakeConst.java
@@ -1,5 +1,9 @@
 package frc.robot.components.intake;
 
 public final class IntakeConst {
+    public static final class Ports{
+        /** IntakeのモーターのCANID */
+        public static final int intakeMotor = 13;
+    }
     
 }
--- src/main/java/frc/robot/components/intake/infrastructure/Intake.java
+++ src/main/java/frc/robot/components/intake/infrastructure/Intake.java
@@ -1,20 +1,48 @@
 package frc.robot.components.intake.infrastructure;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 import frc.robot.domain.repository.IntakeRepository;
+import frc.robot.components.intake.IntakeConst;
+import frc.robot.domain.state.IntakeState;
 
 public class Intake implements IntakeRepository {
+    private final SparkMax IntakeMotor;
+    private final SparkMaxConfig IntakeMotorConfig;
+    private final RelativeEncoder IntakeEncoder;
 
     public Intake() {
-    }
+        IntakeMotor = new SparkMax(IntakeConst.Ports.intakeMotor, MotorType.kBrushless);
+        IntakeMotorConfig = new SparkMaxConfig();
+        IntakeEncoder = IntakeMotor.getEncoder();
 
+        IntakeMotorConfig.inverted(false);
+        IntakeMotorConfig.idleMode(IdleMode.kCoast);
+
+        IntakeMotor.configure(IntakeMotorConfig,ResetMode.kNoResetSafeParameters,PersistMode.kNoPersistParameters);
+    }
+    
+    /**
+     * Intakeを任意のスピードで動かす(PercentOutPut)
+     * @param targetSpeed Intakeを動かすスピード | Fuelを回収する方向が正 | [-1~1] | 止まっているときを0
+     */
     @Override
     public void moveIntakeSpecifiedSpeed(double targetSpeed){
+        IntakeMotor.set(targetSpeed);
     }
     
     /**
      * Stateへの書き込みを行う
      */
     public void periodic() {
+        IntakeState.motorSpeed = IntakeEncoder.getVelocity();
+        IntakeState.isMotorActive = Math.abs(IntakeEncoder.getVelocity()) > 0.1;
     }
     
 }
