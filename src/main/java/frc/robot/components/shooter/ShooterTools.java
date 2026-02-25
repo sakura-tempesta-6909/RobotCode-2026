db52a271d37750463413e905e70659685c73665a
Kanna <doraemon.bd903@gmail.com>
2026-02-25 22:46:24 +0900

    修正と説明

--- src/main/java/frc/robot/components/shooter/ShooterTools.java
+++ src/main/java/frc/robot/components/shooter/ShooterTools.java
@@ -2,8 +2,11 @@ package frc.robot.components.shooter;
 
 public class ShooterTools {
 
-    /** 割合からRPMへ変換　*/
-    public static double RatioToRPM(double supplierRatio) {
-        return supplierRatio * ShooterConst.maxRPM;
+    /** 
+     * 割合からRPMへ変換
+     * @param Ratio モーターを動かす最大RPMに対する割合
+    */
+    public static double RatioToRPM(double Ratio) {
+        return Ratio * ShooterConst.maxRPM;
     }
 }
