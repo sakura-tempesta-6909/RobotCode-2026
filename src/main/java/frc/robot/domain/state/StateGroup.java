507b2f5485608e4866fcb3218db14bef025f71ba
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2026-02-25 21:57:58 +0900

    add currentposition to hib

--- src/main/java/frc/robot/domain/state/StateGroup.java
+++ src/main/java/frc/robot/domain/state/StateGroup.java
@@ -1,5 +1,8 @@
 package frc.robot.domain.state;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.components.drive.DriveParameter;
+
 public class StateGroup {
     public static boolean readyToScore() {
         return true;
@@ -7,5 +10,10 @@ public class StateGroup {
 
     public static boolean readyToShoot() {
         return ShooterState.isReadyToShoot && DriveState.isShootPosition;
+    }
+
+    public static double currentPositionToHub() {
+        Translation2d currentPosition =  DriveState.drivePosition.getTranslation();
+        return currentPosition.minus(DriveParameter.Poses.inFrontOfGoal.getTranslation()).getNorm();
     }
 }
