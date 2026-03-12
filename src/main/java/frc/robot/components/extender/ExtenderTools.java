eaf765126723054f4fabdf61ac2f61271d9207ba
Rei6627 <rei.20100331@gmail.com>
2026-03-12 18:01:33 +0900

    fix ExtenderInfrastructure

--- src/main/java/frc/robot/components/extender/ExtenderTools.java
+++ src/main/java/frc/robot/components/extender/ExtenderTools.java
@@ -1,20 +1,20 @@
 package frc.robot.components.extender;
 
 public class ExtenderTools {
-    /** extenderの回転数に対応するモーターの軸の回転数を求める
+    /** extenderの角度に対応するモーターの軸の回転数を求める
      * @param targetAngle 目標の回転角度[degree]
      * @return モーターの軸の回転数[rotation]
      */
-    public static double getTargetRotationsForMotorShaft(double targetAngle){
+    public static double getRotationsOfMotorShaft(double targetAngle){
         return ExtenderConst.GearRatio * targetAngle /360;
 
     }
 
-    /** extenderの累計回転数に対応するextenderの累計回転数を求める
-     * @param position 累計回転数[回]
-     * @return extenderの累計回転角度[degree]
+    /** extenderの累計回転数に対応するextenderの角度を求める
+     * @param position 回転数[rotation]
+     * @return extenderの回転角度[degree]
      */
-    public static double calcurateRotation(double position){
+    public static double getAngleOfExtender(double position){
         return position * ExtenderConst.GearRatio * 360;
     }
     
