16b022367a63a85dc310587f59d84cbc4bcaf13b
Kanna <doraemon.bd903@gmail.com>
2026-03-15 22:13:34 +0900

    修正

--- src/main/java/frc/robot/components/shooter/ShooterTools.java
+++ src/main/java/frc/robot/components/shooter/ShooterTools.java
@@ -4,9 +4,9 @@ public class ShooterTools {
 
     /** 
      * 距離をもとにシュート時のm/sを算出する 
-     * @param distance 取得したロボットとゴールの距離
-     * @return 距離に応じたモーターの回転数[m/s]
-     */
+     * @param distance 取得したロボットとゴールの距離　単位：m
+     * @return 距離に応じたモーターの回転数[m/s] 30は仮値
+    */
     public static double distanceToMps(double distance) {
         //計算式を入れる
         return 0.0;
