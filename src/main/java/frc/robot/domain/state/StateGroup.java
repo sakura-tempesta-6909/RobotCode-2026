234a2286bdac3e74379d8a4d51f1a76380088b99
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2026-03-15 20:15:26 +0900

    説明かいた

--- src/main/java/frc/robot/domain/state/StateGroup.java
+++ src/main/java/frc/robot/domain/state/StateGroup.java
@@ -8,17 +8,30 @@ public class StateGroup {
     public static boolean readyToScore() {
         return true;
     }
-
+    
+    /**
+     *  Shooterの速度とロボットの位置をもとにシュートできるかどうか
+     * @return　シュートできるかどうか | 可能->true,不可->false 
+     */
     public static boolean readyToShoot() {
         return ShooterState.isReadyToShoot && DriveState.isShootPosition;
     }
 
+    /**
+     * Hubまでの距離を計算する
+     * @return Hubまでの距離 | 単位[m] |
+     */
     public static double getDistanceToHub() {
         Translation2d currentPosition =  DriveState.drivePosition.getTranslation();
         return currentPosition.minus(UsecaseConst.Poses.TargetPoseToHub.getTranslation()).getNorm();
     }
 
+   /**
+    *  ロボットとHubの距離がシュートできる範囲に入っているかどうか
+    * @return 範囲に入っているかどうか | 可能->true,不可->false 
+    */
     public static boolean isShootableRange() {
-        return getDistanceToHub() > DriveParameter.Differences.MinShootableRange && getDistanceToHub() < DriveParameter.Differences.MaxShootableRange;
+        double distance =  getDistanceToHub();
+        return distance > DriveParameter.Differences.MinShootableRange && distance < DriveParameter.Differences.MaxShootableRange;
     }
 }
