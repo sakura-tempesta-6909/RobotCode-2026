e3a1cc1a9f91238682cf59af489e776d2d48d863
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2026-03-18 01:36:13 +0900

    add command

--- src/main/java/frc/robot/auto/AutoCommandConfigure.java
+++ src/main/java/frc/robot/auto/AutoCommandConfigure.java
@@ -7,7 +7,6 @@ import frc.robot.usecase.commands.CommandsGroup;
 public class AutoCommandConfigure {
     public static void registerCommands() {
         NamedCommands.registerCommand("Intake", CommandsGroup.intake());
-        NamedCommands.registerCommand("Score", CommandsGroup.score());
-        NamedCommands.registerCommand("ReadytoScore", CommandsGroup.readytoScore());
+        NamedCommands.registerCommand("shoot", CommandsGroup.shoot());
     }
 }
