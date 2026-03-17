b58210f5a72b53a1e4528b653c948299f67bd106
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2026-03-18 01:45:34 +0900

    終了条件追加

--- src/main/java/frc/robot/auto/AutoCommandConfigure.java
+++ src/main/java/frc/robot/auto/AutoCommandConfigure.java
@@ -2,11 +2,14 @@ package frc.robot.auto;
 
 import com.pathplanner.lib.auto.NamedCommands;
 
+import frc.robot.domain.state.ExtenderState;
 import frc.robot.usecase.commands.CommandsGroup;
+import frc.robot.usecase.commands.ExtenderCommands;
 
 public class AutoCommandConfigure {
     public static void registerCommands() {
-        NamedCommands.registerCommand("Intake", CommandsGroup.intake());
-        NamedCommands.registerCommand("shoot", CommandsGroup.shoot());
+        NamedCommands.registerCommand("Intake", CommandsGroup.intake().withTimeout(5.0));
+        NamedCommands.registerCommand("shoot", CommandsGroup.shoot().withTimeout(7.0));
+        NamedCommands.registerCommand("move to intake angle", ExtenderCommands.moveToIntakeAngle().until(() -> ExtenderState.isIntakePosition));
     }
 }
