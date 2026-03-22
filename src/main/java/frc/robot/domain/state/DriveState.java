70abc3f41e21ff85f118fba5ef70795da2a1de92
Rei6627 <rei.20100331@gmail.com>
2026-03-22 17:58:46 +0900

    fix

--- src/main/java/frc/robot/domain/state/DriveState.java
+++ src/main/java/frc/robot/domain/state/DriveState.java
@@ -4,7 +4,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 public class DriveState {
-    /** スタート地点（青allianceから正面を見て一番右手前）を原点(0,0)としたロボットの位置(x[m],y[m])とフィールド上の前向きを0としたロボットの角度[degree]
+    /** 青allianceから正面を見て一番右手前の角を原点(0,0)としたロボットの位置(x[m],y[m])とフィールド上の前向きを0としたロボットの角度[degree]
      * |x軸:フィールドを青allianceを手前に上から見て奥方向（前方向）を正,Y軸:同じく上から見てフィールドのを左方向を正,角度：ロボットを上から見て反時計回りが正
      * |PathPlannerの座標と一致 (参照→)https://docs.wpilib.org/en/stable/docs/software/basic-programming/coordinate-system.html*/
     public static Pose2d drivePosition;
@@ -12,7 +12,7 @@ public class DriveState {
     public static ChassisSpeeds driveXYOmegaSpeed;
     /** ロボットがシュート可能な位置かどうか |可能->true,不可->false */
     public static boolean isShootPosition;
-    /** スタート地点（青allianceから正面を見て一番右手前）を原点(0,0)としたロボットの目標位置(x[m],y[m])とフィールド上の前向きを0としたロボットの目標角度[degree]
+    /** 青allianceから正面を見て一番右手前の角を原点(0,0)としたロボットの目標位置(x[m],y[m])とフィールド上の前向きを0としたロボットの目標角度[degree]
      * |x軸:青allianceを手前にフィールドを上から見て奥方向（前方向）を正,Y軸:同じく上から見てフィールドのを左方向を正,角度：ロボットを上から見て反時計回りが正
      * |PathPlannerの座標と一致 (参照→)https://docs.wpilib.org/en/stable/docs/software/basic-programming/coordinate-system.html*/
     public static Pose2d targetPosition;
