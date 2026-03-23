6920a35f3025edc332215c60a67e947bdd5c228a
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2026-03-23 20:01:24 +0900

    delete

--- src/main/java/frc/robot/mode/ManualMode.java
+++ src/main/java/frc/robot/mode/ManualMode.java
@@ -90,8 +90,6 @@ public class ManualMode extends Mode {
        operateController.b().whileTrue(CommandsGroup.outtake());
        //feed
        operateController.a().whileTrue(CommandsGroup.feed());
-       operateController.x().whileTrue(IndexerCommands.reverseIndexer());
-       operateController.y().whileTrue(ShooterCommands.reverseShooter());
        //ExtenderのPIDとエンコーダーをリセットする
        operateController.pov(0).onTrue(
         Commands.parallel(
