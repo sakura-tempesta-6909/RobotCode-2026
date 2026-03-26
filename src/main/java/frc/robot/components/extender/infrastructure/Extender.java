46da6dd895e13c2d00ed8cbfecbd9b95de00bb09
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2026-03-26 15:16:05 +0900

    　limit switchに当たった時にエンコーダーをリセットするようにした

--- src/main/java/frc/robot/components/extender/infrastructure/Extender.java
+++ src/main/java/frc/robot/components/extender/infrastructure/Extender.java
@@ -1,31 +1,19 @@
 package frc.robot.components.extender.infrastructure;
 
-import frc.robot.domain.repository.ExtenderRepository;
-import frc.robot.domain.state.ExtenderState;
-import frc.robot.components.extender.ExtenderConst;
-import frc.robot.components.extender.ExtenderTools;
-import frc.robot.components.indexer.IndexerConst;
-import frc.robot.components.indexer.IndexerParameter;
-import frc.robot.components.indexer.IndexerTools;
-import frc.robot.components.extender.ExtenderParameter;
-
-import static edu.wpi.first.units.Units.Percent;
-
-import java.lang.annotation.Target;
-
-import com.ctre.phoenix6.signals.ControlModeValue;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.*;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.LimitSwitchConfig.Type;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
-
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.SparkBaseConfig; // IdleModeに必要
-import com.revrobotics.spark.config.LimitSwitchConfig.Type; // kNormallyOpenに必要
+import frc.robot.components.extender.ExtenderConst;
+import frc.robot.components.extender.ExtenderParameter;
+import frc.robot.components.extender.ExtenderTools;
+import frc.robot.domain.repository.ExtenderRepository;
+import frc.robot.domain.state.ExtenderState;
 
 public class Extender implements ExtenderRepository {
     private final SparkMax extenderMotor;
@@ -87,10 +75,14 @@ public class Extender implements ExtenderRepository {
         /** 底面が地面と平行な場合を0度としたExtenderの角度[degree]|0<=currentAngle<=90|ロボット側に回転するのが正方向*/
         ExtenderState.currentAngle = ExtenderTools.getAngleOfExtender(extenderEncoder.getPosition());
         /** intakeできる位置にExtenderがあるかないか|可能->true,不可->false */
-        ExtenderState.lowerLimit = extenderMotor.getForwardLimitSwitch().isPressed();
+        ExtenderState.lowerLimit = extenderMotor.getReverseLimitSwitch().isPressed();
+        // lowerLimitがtrueのときにエンコーダーをリセット
+        if (ExtenderState.lowerLimit) {
+            extenderEncoder.setPosition(ExtenderTools.getRotationsOfMotorShaft(ExtenderParameter.IntakeAngle));
+        }
         ExtenderState.isIntakePosition = (ExtenderParameter.IntakeAngle - ExtenderParameter.arrowedAngleToJudgeIsInitialAngle < ExtenderState.currentAngle)&&(ExtenderState.currentAngle < ExtenderParameter.InitialAngle + ExtenderParameter.arrowedAngleToJudgeIsInitialAngle);
         /** extenderが初期位置(地面に対して鉛直方向)にあるかどうか|ある->true,ない->false */
-        ExtenderState.upperLimit = extenderMotor.getReverseLimitSwitch().isPressed();
+        ExtenderState.upperLimit = extenderMotor.getForwardLimitSwitch().isPressed();
         ExtenderState.isInitialPosition = (ExtenderParameter.InitialAngle - ExtenderParameter.arrowedAngleToJudgeIsInitialAngle < ExtenderState.currentAngle)&&(ExtenderState.currentAngle < ExtenderParameter.InitialAngle + ExtenderParameter.arrowedAngleToJudgeIsInitialAngle);
         SmartDashboard.putNumber("current angle", ExtenderState.currentAngle);
         SmartDashboard.putNumber("current position", extenderEncoder.getPosition());
