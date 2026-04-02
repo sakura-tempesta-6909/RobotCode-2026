394e5836cd1a7a79e7993d55934de3c913dafd25
zuiken-code <zuiken2022robo@gmail.com>
2026-04-03 01:26:44 +0900

    add led2

--- src/main/java/frc/robot/components/led/LEDConst.java
+++ src/main/java/frc/robot/components/led/LEDConst.java
@@ -2,7 +2,8 @@ package frc.robot.components.led;
 
 public final class LEDConst {
     public final class Ports {
-        public static final int LED = 0;
+        public static final int LED = 9;
+        public static final int LED2 = 8;
     }
-    public static final int LEDLength = 0;
+    public static final int LEDLength = 15;
 }
