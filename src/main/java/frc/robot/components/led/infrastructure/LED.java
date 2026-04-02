fc10d11e29a750f4574c65f639a21e20a6cb4326
zuiken-code <zuiken2022robo@gmail.com>
2026-04-03 03:44:00 +0900

    fix LED

--- src/main/java/frc/robot/components/led/infrastructure/LED.java
+++ src/main/java/frc/robot/components/led/infrastructure/LED.java
@@ -11,16 +11,18 @@ public class LED implements LEDRepository {
     private final AddressableLED led;
     private final AddressableLED led2;
     private final AddressableLEDBuffer ledBuffer;
+    private final AddressableLEDBuffer ledBuffer2;
     private final Timer timer;
     public LED() {
         led =  new AddressableLED(LEDConst.Ports.LED);
         led2 = new AddressableLED(LEDConst.Ports.LED2);
         ledBuffer = new AddressableLEDBuffer(LEDConst.LEDLength);
+        ledBuffer2 = new AddressableLEDBuffer(LEDConst.LEDLength);
         led.setLength(ledBuffer.getLength());
         led.setData(ledBuffer);
         led.start();
-        led2.setLength(ledBuffer.getLength());
-        led2.setData(ledBuffer);
+        led2.setLength(ledBuffer2.getLength());
+        led2.setData(ledBuffer2);
         led2.start();
         timer = new Timer();
         timer.start();
@@ -37,9 +39,10 @@ public class LED implements LEDRepository {
     public void changeLight(int red, int green, int blue) {
         for (var i = 0; i < ledBuffer.getLength(); i++) {
             ledBuffer.setRGB(i, red, green, blue);
+            ledBuffer2.setRGB(i, red, green, blue);
         }
         led.setData(ledBuffer);
-        led2.setData(ledBuffer);
+        led2.setData(ledBuffer2);
     }
 
     @Override
@@ -47,15 +50,18 @@ public class LED implements LEDRepository {
          for (var i = 0; i < ledBuffer.getLength(); i++) {
             if (timer.get() < 0.1) {
                 ledBuffer.setRGB(i, red, green, blue);
+                ledBuffer2.setRGB(i, red, green, blue);
             } else if (timer.get() < 0.2) {
                 ledBuffer.setRGB(i, 0, 0, 0);
+                ledBuffer2.setRGB(i, 0, 0, 0);
             } else {
                 led.start();
+                led2.start();
                 timer.restart();
             }
         }
         led.setData(ledBuffer);
-        led2.setData(ledBuffer);
+        led2.setData(ledBuffer2);
     }
 
     
