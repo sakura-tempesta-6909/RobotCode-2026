cf645e62b507e996d081f0e8e4a4e3b98997f67d
zuiken-code <zuiken2022robo@gmail.com>
2026-04-14 19:35:33 +0900

    delate led2

--- src/main/java/frc/robot/components/led/LEDConst.java
+++ src/main/java/frc/robot/components/led/LEDConst.java
@@ -3,7 +3,6 @@ package frc.robot.components.led;
 public final class LEDConst {
     public final class Ports {
         public static final int LED = 8;
-        public static final int LED2 = 8;
     }
     public static final int LEDLength = 15;
 }
